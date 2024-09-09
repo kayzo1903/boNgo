7e85413148234d5b053bad227143acb34638f7ca
kayzo1903 <kaykinangukammari@gmail.com>
2024-09-09 11:50:17 +0300

    celebration update

--- components/Boo.tsx
+++ components/Boo.tsx
@@ -4,13 +4,13 @@ const Boo = () => {
   return (
     <div className="w-full">
       <h3 className="text-3xl font-extrabold text-center text-yellow-50 leading-relaxed mt-4 animate-bounce">
-        😬 Oops! Better Luck Next Time! 😬
+        😬 Oops Better Luck Next Time{'&#39'} 😬
       </h3>
       <p className="text-2xl text-center text-yellow-100 mt-2">
-        Keep going, you're getting closer! 💪🔥
+        Keep going, you are getting closer 💪🔥
       </p>
       <p className="text-lg text-center text-yellow-100 mt-1">
-        Give it another try and show what you've got! 🚀
+        Give it another try and show what you have got 🚀
       </p>
     </div>
   );
--- components/cerebrationsGPHY.tsx
+++ components/cerebrationsGPHY.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const CerebrationsGPHY = () => {
+const Cerebrations = () => {
   return (
     <div className="w-full">
       <h3 className="text-3xl font-extrabold text-center text-yellow-500 leading-relaxed mt-4 animate-bounce">
-        🎉 YOU DID IT! 🎉
+        🎉 YOU DID IT 🎉
       </h3>
       <p className="text-2xl text-center text-yellow-100 mt-2">
-        You're a memory master! 🧠✨
+        You are a memory master 🧠✨
       </p>
       <p className="text-lg text-center text-yellow-100 mt-1">
-        Get ready for the next challenge! 🚀
+        Get ready for the next challenge 🚀
       </p>
     </div>
   );
 };
 
-export default CerebrationsGPHY;
+export default Cerebrations;
